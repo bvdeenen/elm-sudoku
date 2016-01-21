b8eae03f080874292543d67b36e6f2a9c09eabaa
Bart van Deenen <bart.vandeenen@gmail.com>
2016-01-21 18:28:54 +0100

    NEW handles lines

--- Sudoku.elm
+++ Sudoku.elm
@@ -6,11 +6,13 @@ import Html.Events exposing (onClick)
 import Matrix exposing (..)
 import Set 
 import String
+import Maybe
+import Array
 
 
 -- MODEL
 
-type Cell = Filled Int | Possibles (List Int)
+type Cell = Filled Int | Possibles (List Int) | Bug
 type alias Model = Matrix (Cell)
 
 easy = [ "..48.96.."
@@ -32,35 +34,16 @@ charListToModel lines =
                Possibles [1..9]
            else
                 case String.toInt (String.fromChar c) of
-                    Ok value -> 
-                        Filled value
-                    Err error ->
-                        Possibles [999]
+                    Ok value -> Filled value
+                    Err error -> Bug
     in
        Matrix.fromList ( List.map (\line -> 
            List.map charToCell (String.toList line)
        ) lines)
 
-
-
-
-
 init : Model
 init =
     charListToModel easy
-
-
-init2 : Model
-init2 = Matrix.square 9 (\loc -> 
-    let
-        (r,c) = loc
-    in
-       if r==c then
-          Filled (r + 1)
-       else
-          Possibles [1..9]
-      )
-
 
 subMatrix: (Int,Int) -> (Int,Int) -> Model -> Model
 subMatrix loc size matrix = 
@@ -73,6 +56,53 @@ subMatrix loc size matrix =
     in
        Matrix.fromList newRows
 
+extractColumn: Int -> Matrix a -> List a
+extractColumn c m =
+   if c < 0 || c >= (Matrix.colCount m) then
+      []
+  else
+    List.filterMap ( \rowNr -> Matrix.get (rowNr, c) m) [0..(Matrix.rowCount m)]
+        
+
+filledInValues: List Cell -> Set.Set Int
+filledInValues list = 
+    Set.fromList ( List.filterMap (\v -> 
+        case v of
+            Filled value -> Just value
+            Possibles _ -> Nothing
+            Bug -> Nothing
+        ) list)
+
+removePossiblesFromLines: Model -> Model
+removePossiblesFromLines model =
+    let
+        filledInValues' array = filledInValues (Array.toList array)
+        -- rowValues and columnValues are arrays of Set Int
+        rowValues = Array.map (\rowArray -> filledInValues' rowArray) model
+        columnValues = Array.fromList ( List.map (\colNr -> 
+            filledInValues  (extractColumn colNr model) 
+        ) [0..(Matrix.colCount model)])
+        filter location el =
+            let
+                (r,c)=location
+            in
+               case el of
+                   Filled _ ->
+                       el
+                   Possibles possibles ->
+                       let
+                           possiblesSet = Set.fromList possibles
+                           rowSet = Maybe.withDefault Set.empty (Array.get r rowValues)
+                           colSet = Maybe.withDefault Set.empty (Array.get c columnValues)
+                           allSet = Set.union rowSet colSet
+                       in
+                           Possibles (Set.toList (Set.diff possiblesSet allSet))
+                   Bug ->
+                       Bug
+    in
+       Matrix.mapWithLocation filter model
+
+
 removePossiblesFromSquares: Model -> Model
 removePossiblesFromSquares model = 
     let
@@ -81,13 +111,9 @@ removePossiblesFromSquares model =
                 (r,c)=loc
                 pos = (3*r, 3*c)
                 matrix3x3 = subMatrix pos (3,3) model
-                values = List.filterMap (\v -> 
-                    case v of
-                        Filled value -> Just value
-                        Possibles _ -> Nothing
-                    ) (Matrix.flatten matrix3x3)
+                values = filledInValues (Matrix.flatten matrix3x3)
             in
-               (matrix3x3, (Set.fromList values))
+               (matrix3x3, values)
            )
         filter location el =
             let
@@ -104,11 +130,9 @@ removePossiblesFromSquares model =
                                 Just (_, values) ->
                                    Possibles (Set.toList (Set.diff possiblesSet values))
                                 Nothing ->
-                                   Possibles [99]
-
-
-
-
+                                   Bug
+                   Bug ->
+                       Bug
     in
        Matrix.mapWithLocation filter model
 
@@ -116,13 +140,15 @@ removePossiblesFromSquares model =
 
 -- UPDATE
 
-type Action = RemovePossiblesFromSquare
+type Action = RemovePossiblesFromSquare | RemovePossiblesFromLines
 
 update : Action -> Model -> Model
 update action model =
   case action of
     RemovePossiblesFromSquare ->
       removePossiblesFromSquares model
+    RemovePossiblesFromLines ->
+      removePossiblesFromLines model
 
 
 -- VIEW
@@ -144,12 +170,17 @@ view address model =
                         span [class ("Possible p" ++ (toString p))] [
                             text (toString p)
                         ]) possibles)
+                Bug ->
+                    span [class "content Bug"] [
+                        text "BUG"
+                    ]
             ]
         htmlRow row = div [] (List.map oneCell row)
     in
        div [] 
          [ div [class "sudoku"] (List.map htmlRow (rows model))
          , button [ onClick address RemovePossiblesFromSquare ] [ text "#" ]
+         , button [ onClick address RemovePossiblesFromLines ] [ text "L" ]
          ]
 
 
