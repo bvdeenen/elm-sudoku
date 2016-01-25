51f8e984bb490c03215ad1bffb524d83910b2aa1
Bart van Deenen <bart.vandeenen@gmail.com>
2016-01-25 16:08:08 +0100

    Revert "NEW algorithm to find unique sets of 2 or more in a group"
    This reverts commit 1e1cfc82a6a75f43a272c191fbcb46aa5f476525.

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ PACKAGE_FLAGS = -y
 BUILD_DIR = build
 RESOURCES_DIR = resources
 
-.PHONY: test quickcheck publish
+.PHONY: test quickcheck
 
 all: package/install clean compile
 
@@ -39,7 +39,3 @@ test:
 
 quickcheck:
 	$(CC) test/TestRunner.elm --output build/quickcheck.html
-
-publish:
-	cp style.css bvdeenen.github.io/sudoku
-	cp build/elm.js bvdeenen.github.io/sudoku/build
--- src/Utils.elm
+++ src/Utils.elm
@@ -23,39 +23,14 @@ reverseMap tupleList =
         ) accu values
     ) Dict.empty tupleList
 
-findMultiples: List (comparable', Set.Set comparable) -> Dict.Dict (List comparable) (Set.Set comparable')
-findMultiples tupleList =
-    let
-        groups = List.map (\(val,set) ->
-            (val, Set.singleton (Set.toList set))
-        ) (reverseMap tupleList |> Dict.toList)
-
-        groupedPositions: Dict.Dict (List comparable) (Set.Set comparable')
-        groupedPositions = reverseMap groups
-    in
-        Dict.filter (\positions values ->
-            (List.length positions) == (Set.size values)
-        ) groupedPositions
-
-
-findMultiples': List (comparable', Set.Set comparable) -> Int -> Dict.Dict (List comparable) (Set.Set comparable')
-findMultiples' tupleList ct =
-    findMultiples tupleList |> Dict.filter (\pos _ -> (List.length pos) == ct)
-
 findSingles:  List (comparable', Set.Set comparable) -> List (comparable', comparable)
-findSingles tupleList = 
-    let 
-        singles = findMultiples' tupleList 1 |> Dict.toList
-    in
-       singles |> List.filterMap (\(k,v)->
-        let 
-            k' = List.head k
-            v' = Set.toList v |> List.head
-        in
-           case (k',v') of
-               (Just n, Just m) -> Just (n,m)
-               _ -> Nothing
-           ) 
+findSingles tupleList =
+    Dict.foldl (\val locations accu ->
+        case Set.toList locations of 
+            [loc] ->
+                (loc, val) :: accu
+            _ ->
+                accu) [] (reverseMap tupleList) 
 
 
 
